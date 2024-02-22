6836ee07e09abcb05fe9d3b537a31476f436ba97
very john <armaanjelly@gmail.com>
2024-02-21 17:49:29 -0800

    make some modification

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
-      process.env.DB_PW,
+      process.env.DB_PASSWORD,
       {
         host: "localhost",
         dialect: "mysql",
@@ -21,3 +21,7 @@ if (process.env.JAWSDB_URL) {
   }
   
   module.exports = sequelize;
+
+  //to install a specific npm module version - npm i express@3.2.4 
+  //https://www.youtube.com/watch?v=3i4pc3iWjqQ
+  //https://www.youtube.com/watch?v=CXiT-DWn1zs
